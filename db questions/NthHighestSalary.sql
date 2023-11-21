7c9a3553a34c2f681135bdaa0d6ff3e05d7c20cb
Yashwanth S <yashwanth.syc@gmail.com>
2023-11-21 13:46:16 +0530

    Implemented NthHighestSalary.sql
    Solved : "NthHighestSalary"
    LeetCode Id : 177
    Date : 21-11-2023
    Difficulty: Medium

---
+++ db questions/NthHighestSalary.sql
@@ -0,0 +1,78 @@
+/*
+
+LeetCode Id : 177
+------------------------
+Problem Description
+
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+Write a solution to find the nth highest salary from the Employee table. If there is no nth highest salary, return null.
+The result format is in the following example.
+
+Example 1:
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+
+Example 2:
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| null                   |
++------------------------+
+
+--------------------------------------------------------------------------------------------
+SQL SCHEMA
+
+Create table If Not Exists Employee (Id int, Salary int)
+Truncate table Employee
+insert into Employee (id, salary) values ('1', '100')
+insert into Employee (id, salary) values ('2', '200')
+insert into Employee (id, salary) values ('3', '300')
+---------------------------------------------------------------------------------------------------
+
+*/
+
+# Write your MySQL query statement below
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+     SET N = N - 1;
+    
+    RETURN (
+        SELECT DISTINCT Salary 
+        FROM Employee 
+        ORDER BY Salary DESC 
+        LIMIT N, 1
+    );
+END;
