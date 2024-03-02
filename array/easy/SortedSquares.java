93031d30f54ed4639e629e7b0a22d76c646db787
Yashwanth S <yashwanth.syc@gmail.com>
2024-03-02 20:09:20 +0530

    Implemented SortedSquares.java
    Solved "SortedSquares"
    LeetCode Id: 977
    Difficulty: Easy
    Date: 03-02-2024

---
+++ array/easy/SortedSquares.java
@@ -0,0 +1,55 @@
+/* 
+LeetCode Id : 977
+
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+Example 1:
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+
+Example 2:
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ 
+Constraints:
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in non-decreasing order.
+
+Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+
+TC : O(N), SC : O(N)
+
+Approach: The method squares each number in the input array and stores the squares in another array. 
+It compares squares of numbers from both ends of the array, placing the larger square in the result array. 
+This ensures the squares are sorted in non-decreasing order.
+
+*/
+public class Solution {
+    public static int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        int start = 0, end = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (Math.abs(nums[start]) >= Math.abs(nums[end])) {
+                ans[i] = nums[start] * nums[start];
+                start++;
+            } else {
+                ans[i] = nums[end] * nums[end];
+                end--;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-4, -1, 0, 3, 10};
+        int[] result = sortedSquares(nums);
+        System.out.print("Sorted squares: ");
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
